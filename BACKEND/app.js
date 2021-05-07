2298d2a115970ec5b307a25c70356b4837f5d2b3
yitzhak-bloy <ik.bd.93@gmail.com>
2021-05-07 16:37:21 +0300

    Arrange all code in backend with Prettier

--- BACKEND/app.js
+++ BACKEND/app.js
@@ -1,42 +1,43 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
-const HttpError = require('./models/http-error');
-const statisticsRoutes = require('./routes/userStatistics-routes');
+const HttpError = require("./models/http-error");
+const statisticsRoutes = require("./routes/userStatistics-routes");
 
 const app = express();
 
 app.use(bodyParser.json());
 
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
   );
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH');
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH");
 
   next();
 });
 
-app.use('/api/userStatistics', statisticsRoutes);
+app.use("/api/userStatistics", statisticsRoutes);
 
 app.use((req, res, next) => {
-  const error = new HttpError('Could not find this route', 404);
-  throw error
-})
+  const error = new HttpError("Could not find this route", 404);
+  throw error;
+});
 
 app.use((error, req, res, next) => {
   if (res.headerSent) {
     return next(error);
   }
   res.status(error.code || 500);
-  res.json({ message: error.message || 'An unknown error occurred!' });
+  res.json({ message: error.message || "An unknown error occurred!" });
 });
 
 mongoose
-  .connect('mongodb+srv://yitzhak:nkfh1993@cluster0.3y1b4.mongodb.net/userStatistics?retryWrites=true&w=majority')
+  .connect(
+    "mongodb+srv://yitzhak:nkfh1993@cluster0.3y1b4.mongodb.net/userStatistics?retryWrites=true&w=majority"
+  )
   .then(() => app.listen(5000))
-  .catch(err => console.log(err))
-
+  .catch((err) => console.log(err));
--- BACKEND/controllers/snakeStatistics-controllers.js
+++ BACKEND/controllers/snakeStatistics-controllers.js
@@ -1,38 +1,40 @@
-const { validationResult } = require('express-validator');
+const { validationResult } = require("express-validator");
 
-const HttpError = require('../models/http-error');
-const UserStatistc = require('../models/userStatistic');
+const HttpError = require("../models/http-error");
+const UserStatistc = require("../models/userStatistic");
 
 const updateSnakeStatistics = async (req, res, next) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
     console.log("errors:", errors);
-    throw new HttpError('Something went wrong.', 422);
-  };
+    throw new HttpError("Something went wrong.", 422);
+  }
 
   const { email, level, result } = req.body;
 
   let UserStatistic;
   try {
-    UserStatistic = await UserStatistc.findOne({ email: email })
+    UserStatistic = await UserStatistc.findOne({ email: email });
   } catch (err) {
-    return next(new HttpError('Samething went wrong, please try again', 404))
+    return next(new HttpError("Samething went wrong, please try again", 404));
   }
 
   if (!UserStatistic) {
-    return next(new HttpError('could not find a user for the provided id', 404));
+    return next(
+      new HttpError("could not find a user for the provided id", 404)
+    );
   }
 
   if (UserStatistic.snakeStatistic[level] < result) {
-    UserStatistic.snakeStatistic[level] = result
+    UserStatistic.snakeStatistic[level] = result;
 
     try {
       await UserStatistic.save();
     } catch (err) {
       const error = new HttpError(
-        'Update snake user statistic failed, please try again.',
+        "Update snake user statistic failed, please try again.",
         500
-      )
+      );
       return next(error);
     }
   }
