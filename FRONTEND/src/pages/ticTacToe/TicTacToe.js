ec07021e16781c7ba97144e487edc222274c6e8f
yitzhak-bloy <ik.bd.93@gmail.com>
2021-06-11 11:44:34 +0300

    Separating the UserPlayerProvider into a separate component that also manages the state within it.

--- FRONTEND/src/pages/ticTacToe/TicTacToe.js
+++ FRONTEND/src/pages/ticTacToe/TicTacToe.js
@@ -1,43 +1,20 @@
-import { useState, useReducer } from "react";
+import { useState } from "react";
 import Box from "@material-ui/core/Box";
 
 import TicTacToeBoard from "../../components/ticTacToe-components/ticTacToeBoard/TicTacToeBoard";
 import SelectPlayer from "../../components/ticTacToe-components/selectPlayer/SelectPlayer";
 import SelectLevel from "../../components/ticTacToe-components/selectLevel/SelectLevel";
-import { PlayerContext } from "../../shared/context/Player-context";
+import { UserPlayerProvider } from "../../shared/context/Player-context";
 import { DifficultyLevelContext } from "../../shared/context/DifficultyLevel-context";
 import { GameRunning } from "../../shared/context/GameRunning-context";
 import { TurnContext } from "../../shared/context/Turn-context";
 import "./TicTacToe.css";
 import { Grid } from "@material-ui/core";
 
-const reducerPlayer = (state, action) => {
-  if (action.type === "X") {
-    return {
-      huPlayer: "X",
-      aiPlayer: "O",
-    };
-  } else if (action.type === "O") {
-    return {
-      huPlayer: "O",
-      aiPlayer: "X",
-    };
-  }
-};
-
 const TicTacToe = () => {
   const [level, setLevel] = useState("hard");
   const [gameRunning, setGameRunning] = useState(false);
   const [whoseTurn, setWhoseTurn] = useState("X");
-
-  const [statePlayer, dispatchPlayer] = useReducer(reducerPlayer, {
-    huPlayer: "X",
-    aiPlayer: "O",
-  });
-
-  const playerChangeHandler = (player) => {
-    dispatchPlayer({ type: player });
-  };
 
   return (
     <GameRunning.Provider
@@ -48,13 +25,7 @@ const TicTacToe = () => {
         },
       }}
     >
-      <PlayerContext.Provider
-        value={{
-          huPlayer: statePlayer.huPlayer,
-          aiPlayer: statePlayer.aiPlayer,
-          playerChangeHandler: playerChangeHandler,
-        }}
-      >
+      <UserPlayerProvider>
         <DifficultyLevelContext.Provider
           value={{
             difficultyLevel: level,
@@ -84,7 +55,7 @@ const TicTacToe = () => {
             </Box>
           </TurnContext.Provider>
         </DifficultyLevelContext.Provider>
-      </PlayerContext.Provider>
+      </UserPlayerProvider>
     </GameRunning.Provider>
   );
 };
--- FRONTEND/src/shared/context/Player-context.js
+++ FRONTEND/src/shared/context/Player-context.js
@@ -1,7 +1,44 @@
-import { createContext } from "react";
+import { useReducer, createContext } from "react";
 
 export const PlayerContext = createContext({
   huPlayer: "X",
   aiPlayer: "O",
   playerChangeHandler: () => {},
 });
+
+const reducerPlayer = (state, action) => {
+  if (action.type === "X") {
+    return {
+      huPlayer: "X",
+      aiPlayer: "O",
+    };
+  } else if (action.type === "O") {
+    return {
+      huPlayer: "O",
+      aiPlayer: "X",
+    };
+  }
+};
+
+export const UserPlayerProvider = (props) => {
+  const [statePlayer, dispatchPlayer] = useReducer(reducerPlayer, {
+    huPlayer: "X",
+    aiPlayer: "O",
+  });
+
+  const playerChangeHandler = (player) => {
+    dispatchPlayer({ type: player });
+  };
+
+  return (
+    <PlayerContext.Provider
+      value={{
+        huPlayer: statePlayer.huPlayer,
+        aiPlayer: statePlayer.aiPlayer,
+        playerChangeHandler: playerChangeHandler,
+      }}
+    >
+      {props.children}
+    </PlayerContext.Provider>
+  );
+};
