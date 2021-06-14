d14aa3db4981bd5acfa8973ebf94a2e4499ea539
yitzhak-bloy <ik.bd.93@gmail.com>
2021-06-14 21:58:33 +0300

    Continue designing the CardGame

--- FRONTEND/src/components/homepage-components/cord-game/CardGame.js
+++ FRONTEND/src/components/homepage-components/cord-game/CardGame.js
@@ -6,7 +6,7 @@ import { Link } from "@material-ui/core";
 const CardGame = ({ link, name }) => {
   return (
     <a className='card__game' component={RouterLink} href={link}>
-      <div className='imag-card__game'></div>
+      <div className={`imag-card__game image-${name}`}></div>
       <div className='name-card__game'>{name}</div>
     </a>
   );
